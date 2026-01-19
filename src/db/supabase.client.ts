99c10ded49ccd8cc7549daabbc70acb38b0db755
Marcin Krzyszkowski <marcin.krzyszkowski@gmail.com>
2026-01-19 22:50:50 +0100

    fix: ESLint formatting

--- src/db/supabase.client.ts
+++ src/db/supabase.client.ts
@@ -1,6 +1,6 @@
-import {createClient} from "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js";
 
-import type {Database} from "./database.types.ts";
+import type { Database } from "./database.types.ts";
 
 const supabaseUrl = import.meta.env.SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.SUPABASE_KEY;
